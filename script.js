51cca8f58259dcc9e2d9b4d0c5422861af154a31
unknown <eskicmihajlo55@gmail.com>
2024-02-13 18:28:08 +0100

    started with js

--- script.js
+++ script.js
@@ -1,12 +1,82 @@
 const taskForm = document.getElementById("task-form");  /// Form1
 const openTaskFormBtn = document.getElementById("open-task-form-btn");     /// ADD NEW TASK BTN
 const closeTaskFormBtn = document.getElementById("close-task-form-btn");   /// CLOSE FORM BTN
-const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn")   /// ADD FORM  BTN 
-const titleInput = document.getElementById("title-input")   /// INPUT TASK 
+const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");   /// ADD FORM  BTN 
+
+const titleInput = document.getElementById("title-input");   /// INPUT TASK 
+const dateInput = document.getElementById("date-input");    /// INPUT DATE
+const descriptionInput = document.getElementById("description-input");
 
 const confirmCloseDialog = document.getElementById("confirm-close-dialog");  // dialog
-const cancelBtn = document.getElementById("cancel-btn")   /// cancel (dialog)
+const cancelBtn = document.getElementById("cancel-btn") ;  /// cancel (dialog)
 const discardBtn = document.getElementById("discard-btn");  /// discard (dialog)
 
 
 const tasksContainer = document.getElementById("tasks-container");    // output
+
+
+const taskData  = [];
+let currentTask = {};
+
+
+
+openTaskFormBtn.addEventListener("click", ()=>{
+
+
+    taskForm.classList.toggle("hidden");
+
+
+
+})
+
+
+closeTaskFormBtn.addEventListener("click", ()=> {
+
+    confirmCloseDialog.showModal();
+    
+    cancelBtn.addEventListener("click", ()=> {
+        
+        confirmCloseDialog.close();
+
+    });
+
+    discardBtn.addEventListener("click", ()=> {
+
+
+        confirmCloseDialog.close();
+        taskForm.classList.toggle("hidden");
+
+    })
+
+
+
+
+})
+
+
+taskForm.addEventListener("submit" , (e)=> {
+
+    e.preventDefault()
+
+    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);  // if element exist it will return his index 
+    // returns -1 if results in not found
+    
+    const taskObj = {
+
+    id: `${titleInput.value.toLowerCase().split(' ').join('-')}-${Date.now()}`,  // Foramting id 
+    title : titleInput.value,
+    date : dateInput.value,
+    description : descriptionInput.value
+
+    };
+
+    if (dataArrIndex === -1)
+    {
+        taskData.unshift(taskObj);
+
+    }
+
+
+   // console.log(taskData);
+
+})
